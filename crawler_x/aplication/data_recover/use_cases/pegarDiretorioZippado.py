61b1959102f2570d97ee4091c7f8b057d5e6a913
nataferreiradev <nataferreira929@hotmail.com>
2025-05-24 16:55:03 -0300

    feat: add functionality to delete zip files before zipping directory

--- crawler_x/aplication/data_recover/use_cases/pegarDiretorioZippado.py
+++ crawler_x/aplication/data_recover/use_cases/pegarDiretorioZippado.py
@@ -5,4 +5,5 @@ class PegarDiretorioZippado:
         self.data_recover = DataRecover()
         
     def execute(self,directory_name):
+        self.data_recover.delete_zip_files()
         return self.data_recover.zip_directory_by_name(directory_name)
